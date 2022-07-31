bf70d300b4a52928a8d0ab64a187c71eec6fa613
Semilael <ariana-ses97@yandex.ru>
2022-08-01 00:11:38 +0300

    Created a prog to multiply A B-times

---
+++ NumInDegree/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Данная программа принимает на вход два числа (A и B) и возводит число A в натуральную степень B. \nВведите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2e число: ");
+int degree = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{num} ^ {degree} = " + Math.Pow(num, degree));
