5142954680b49514e1f7d5f77c8e722b50ac97ad
Semilael <ariana-ses97@yandex.ru>
2022-08-01 00:12:24 +0300

    Created a prog sum un digits in a number

---
+++ SumNum/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+while (num > 0)
+{
+    result += num % 10;
+    num = num / 10;
+}
+Console.WriteLine($"Сумма чисел:  {result}");
